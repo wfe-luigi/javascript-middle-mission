b065ae374c35ceae39cf5fc51c816e9f0c2f3a34
wfe-luigi <kdh3979@naver.com>
2017-10-19 15:34:21 +0900

    중첩된 JSON 데이터조작

---
+++ mission2.js
@@ -0,0 +1,92 @@
+//아래와 같이 동작하는 parse 함수를 구현.
+//출력된 name은 모두 'sk' 타입을 가진 사람들이다.
+//obj https://gist.github.com/nigayo/a9a118977f82780441db664d6785efe3
+var result = [];
+var widget = [{
+    "id": 1,
+    "name": "Yong",
+    "phone": "010-2786-9902",
+    "type": "sk",
+    "childnode": [{
+        "id": 11,
+        "name": "echo",
+        "phone": "010-3923-1333",
+        "type": "kt",
+        "childnode": [{
+            "id": 115,
+            "name": "hary",
+            "phone": "010-2786-9302",
+            "type": "sk",
+            "childnode": [{
+                "id": 1159,
+                "name": "pobi",
+                "phone": "010-9302-0009",
+                "type": "kt",
+                "childnode": [{
+                    "id": 11592,
+                    "name": "cherry",
+                    "phone": "010-1223-9932",
+                    "type": "lg",
+                    "childnode": []
+                },
+                {
+                    "id": 11595,
+                    "name": "solvin",
+                    "phone": "010-534-7843",
+                    "type": "sk",
+                    "childnode": []
+                }
+                ]
+            }]
+        },
+        {
+            "id": 116,
+            "name": "kim",
+            "phone": "010-3796-1102",
+            "type": "kt",
+            "childnode": [{
+                "id": 1168,
+                "name": "hani",
+                "phone": "010-1223-6713",
+                "type": "sk",
+                "childnode": [{
+                    "id": 11689,
+                    "name": "ho",
+                    "phone": "010-4434-4534",
+                    "type": "kt",
+                    "childnode": [{
+                        "id": 116890,
+                        "name": "wonsuk",
+                        "phone": "010-3434-1302",
+                        "type": "kt",
+                        "childnode": []
+                    },
+                    {
+                        "id": 1168901,
+                        "name": "chulsu",
+                        "phone": "010-3100-9841",
+                        "type": "sk",
+                        "childnode": []
+                    }
+                    ]
+                }]
+            }]
+        },
+        {
+            "id": 117,
+            "name": "hong",
+            "phone": "010-2786-9902",
+            "type": "lg",
+            "childnode": []
+        }
+        ]
+    }]
+}]
+function getNumber(obj) {
+    for (property in obj) {
+        if (obj[property] == 'sk') result.push(obj['name']);
+        else if (typeof (obj[property]) == 'object') getNumber(obj[property])
+    }
+}
+getNumber(widget);
+console.log(result);
